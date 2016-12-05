a36c674c7452651a6b75090419cc45a7ded288c7
Thanh Pham <thanhpk@live.com>
2016-12-05 23:53:57 +0700

    support include

--- index.js
+++ index.js
@@ -4,20 +4,46 @@ var fs = require('fs');
 var destfile = process.argv[2];
 var validUrl = require('valid-url');
 var async = require('async');
-
+var _ = require('lodash');
+var path = require('path');
 function parse(content, cb) {
 	var ymlobj = yaml.parse(content);
 	var namespaceMap = ymlobj.includes;
-	async.forEach(Object.keys(ymlobj.includes), function(namespace, callback) {
-		if (validUrl.isUri(ymlobj.includes[namespace])) {
-			request(ymlobj.includes[namespace], function(err, response, body) {
-				parse(body, callback);
+
+	if (ymlobj.includes == undefined) {
+		cb(ymlobj.services);
+		return;
+	}
+
+	
+	async.filter(Object.keys(ymlobj.includes), function(namespace, callback) {
+		console.log(namespace);
+		async.waterfall([function(callback) {
+			if (validUrl.isUri(ymlobj.includes[namespace])) {
+				request(ymlobj.includes[namespace], function(err, response, body) {
+					parse(body, callback);
+				});
+			} else {
+				
+				process.chdir(path.dirname(destfile));
+				fs.readFile(ymlobj.includes[namespace], 'utf8', function(err, data) {
+					if (err) throw err;
+					parse(data, callback);
+				});
+			}
+		}], function(childServices) {			
+			if (childServices == undefined) {
+				callback(undefined);
+				return;
+			}
+			var services = {};
+			_.map(Object.keys(childServices), function(servicename) {
+				services[`${namespace}.${servicename}`] = childServices[servicename];
 			});
-		} else {
-			callback();
-		}
-	}, function(err) {
-		console.log('done');
+			callback(services);
+		});
+	}, function(servicesArr) {
+		console.log(servicesArr);
 	});
 }
 
