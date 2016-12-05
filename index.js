fcaf31ad7eca76b2bb5f84ca0df015a3d6fabfa3
Thanh Pham <thanhpk@live.com>
2016-12-05 22:29:51 +0700

    parse yml

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-var yaml = require('node-yaml');
-
+var yaml = require('yamljs');
+var request = require('request');
+var fs = require('fs');
 var destfile = process.argv[2];
+var validUrl = require('valid-url');
+var async = require('async');
 
-console.log(destfile);
-yaml.read(destfile, 'utf8', yaml.schema.defaultSafe, function(err, data) {
-	if (err) throw err;
-	console.log(data);
+function parse(content, cb) {
+	var ymlobj = yaml.parse(content);
+	var namespaceMap = ymlobj.includes;
+	async.forEach(Object.keys(ymlobj.includes), function(namespace, callback) {
+		if (validUrl.isUri(ymlobj.includes[namespace])) {
+			request(ymlobj.includes[namespace], function(err, response, body) {
+				parse(body, callback);
+			});
+		} else {
+			callback();
+		}
+	}, function(err) {
+		console.log('done');
+	});
+}
+
+if (validUrl.isUri(destfile)) {
+	request(destfile, function(err, response, body) {
+		parse(body, function(){});
+	});
+} else {
+	fs.readFile(destfile, 'utf8', function(err, data) {
+		if (err) throw err;
+		parse(data, function(){});
+	});
 }
