5448456c02719fed984925f130d1eae1f6cf35d1
Pham Kieu Thanh <thanhpk@live.com>
2016-12-22 11:10:22 +0700

    fix assert

--- test/test_swarm.js
+++ test/test_swarm.js
@@ -8,8 +8,8 @@ describe('Swarm', function() {
 	describe('servicename', function() {
 		it('should contains scope', function(done) {
 			swarm.parse(path.resolve(__dirname, './servicename/docker-compose.yml'), 'thanhpk', function(output) {
-				assert.Equal(output.indexOf('--name thanhpk.service1'), -1);
-				assert.Equal(output.indexOf('--name thanhpk.B.service2'), -1);
+				assert.notEqual(output.indexOf('--name thanhpk.service1'), -1);
+				assert.notEqual(output.indexOf('--name thanhpk.B.service2'), -1);
 				done();
 			});
 		});
@@ -18,8 +18,8 @@ describe('Swarm', function() {
 	describe('enviroments', function() {			
 		it('should add --env or -e based on environment', function(done) {
 			swarm.parse(path.resolve(__dirname, './swarm/docker-compose.yml'), 'thanhpk', function(output) {
-				assert.Equal(output.indexOf('--env \'thanh=1234\'') != -1 || output.indexOf('-e \'thanh=1234\'') != -1, true);
-				assert.Equal(output.indexOf('--env \'van=Hi llo\'') != 1 || output.indexOf('-e \'van=Hi llo\'') != -1, true);
+				assert.equal(output.indexOf('--env \'thanh=1234\'') != -1 || output.indexOf('-e \'thanh=1234\'') != -1, true);
+				assert.equal(output.indexOf('--env \'van=Hi llo\'') != 1 || output.indexOf('-e \'van=Hi llo\'') != -1, true);
 				done();
 			});
 		});
@@ -28,7 +28,7 @@ describe('Swarm', function() {
 	describe('expose', function() {
 		it('should add --publish based on expose', function(done) {
 			swarm.parse(path.resolve(__dirname, './swarm/docker-compose.yml'), 'thanhpk', function(output) {
-				assert.Equal(output.indexOf('--publish 8080') != -1 || output.indexOf('-p 8080') != -1, true);
+				assert.equal(output.indexOf('--publish 8080') != -1 || output.indexOf('-p 8080') != -1, true);
 				done();
 			});
 		});
@@ -37,17 +37,17 @@ describe('Swarm', function() {
 	describe('network', function() {
 		it('should add --network based on scope', function(done) {
 			swarm.parse(path.resolve(__dirname, './swarm/docker-compose.yml'), 'thanhpk', function(output) {
-				assert.Equal(output.indexOf('--network thanhpk_overlay_ds'), -1);
+				assert.notEqual(output.indexOf('--network thanhpk_overlay_ds'), -1);
 				done();
 			});
 		});
 	});
 
-	descible('volume', function() {
+	describe('volume', function() {
 		it('should add volume', function(done) {
 			swarm.parse(path.resolve(__dirname, './swarm/docker-compose.yml'), 'thanhpk', function(output) {
-				assert.Equal("--mount type=bind,source=/tmp/thanhpk/mongo/data/db,destination=/data/db", -1);
-				assert.Equal("--mount type=bind,source=/tmp/thanhpk/mongo/data/db2,destination=/data/db2", -1);
+				assert.notEqual(output.indexOf("--mount type=bind,source=/tmp/thanhpk/mongo/data/db,destination=/data/db"), -1);
+				assert.notEqual(output.indexOf("--mount type=bind,source=/tmp/thanhpk/mongo/data/db2,destination=/data/db2"), -1);
 				done();
 			});
 		});
