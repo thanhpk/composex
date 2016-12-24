722aaca988d46c876cdc7bb720c74f134a72bf5f
Pham Kieu Thanh <thanhpk@live.com>
2016-12-24 13:39:11 +0700

    change links to includes

--- test/test_importexports.js
+++ test/test_importexports.js
@@ -4,10 +4,10 @@ var composex = require('../index.js');
 var yml = require('yamljs');
 var path = require('path');
 
-describle('Version2', function() {
+describle('Import & Export', function() {
 	describe('import export', function() {
 		it('should replace the host_env with name in import list', function(done) {
-			composex.parse2(__dirname + '/importexport/importer.yml', function(obj) {
+			composex.parse(__dirname + '/importexport/importer.yml', function(obj) {
 				assert.notEqual(obj.services.webserver.environment.join('\n').indexOf('DATABASE_HOST=E.mysql'), -1);
 				assert.netEqual(obj.services.webserver.environment.join('\n').indexOf('DATABASE_PORT=3309'), -1);
 				done();
@@ -15,17 +15,17 @@ describle('Version2', function() {
 		});
 
 		it('should have correct name when being linked', function(done) {
-			composex.parse2(__dirname + '/importexport/indirectimport.yml', function(obj) {
+			composex.parse(__dirname + '/importexport/indirectimport.yml', function(obj) {
 				assert.notEqual(obj.services.webserver.environment.join('\n').indexOf('DATABASE_HOST=I.E.mysql'), -1);
 				assert.notEqual(obj.services.webserver.environment.join('\n').indexOf('DATABASE_PORT=3309'), -1);
 				done();
 			});
 		});
 	});
-	
+
 	describle('join', function() {
 		it('should replace the env', function (done) {
-			composex.parse2(__dirname + '/join/join.yml', function(obj) {
+			composex.parse(__dirname + '/join/join.yml', function(obj) {
 				assert.notEqual(obj.services.A.webserver.environment.join('\n').indexOf('DBHOST=B.mysql'), -1);
 				assert.notEqual(obj.services.A.webserver.environment.join('\n').indexOf('DBPORT=3306'), -1);				
 			});
